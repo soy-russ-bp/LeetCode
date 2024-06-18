bf4b81abf3dc0f083587e4d00e66ad57287b66bd
Russel Bonilla <112036753+soy-russ-bp@users.noreply.github.com>
2024-06-18 01:17:31 -0600

    Time: 2 ms (61.79%), Space: 41.2 MB (75.30%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,24 +1,20 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        char pointerT=' ';
-        int count=0;
-            if(s.isEmpty()){
-                return true;  
-            }
-            int i=0;
-            for(int j=0; j<t.length();j++){
-                
-                s.charAt(i);
-                pointerT=t.charAt(j);
-                
-                if(pointerT==s.charAt(i)){
-                    count++;
-                    System.out.println(count);
-                    i++;
+        if (s.isEmpty()) {
+            return true;
+        }
+
+        int i = 0; // Puntero para `s`
+
+        for (int j = 0; j < t.length(); j++) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+                if (i == s.length()) {
+                    return true;//Se han encontrado todos los caracteres de `s` en`t`
                 }
-                if(count==s.length()){return true;}
             }
-        
-        return false;
+        }
+
+        return false; // No se han encontrado todos los caracteres de `s` en `t`
     }
 }
