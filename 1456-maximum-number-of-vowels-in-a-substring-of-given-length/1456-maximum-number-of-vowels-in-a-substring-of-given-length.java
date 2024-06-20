81a4ca1abe952981537b7d5af3279c39a452d942
Russel Bonilla <112036753+soy-russ-bp@users.noreply.github.com>
2024-06-20 00:15:04 -0600

    Time: 20 ms (43.79%), Space: 44.3 MB (85.54%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -3,14 +3,12 @@ class Solution {
         int maxNumVowel = 0;
         int countNumVowel = 0;
         
-        // Cuenta las vocales en la primera ventana de longitud k
         for (int i = 0; i < k; i++) {
             if (isVowel(s.charAt(i))) {
                 countNumVowel++;
             }
         }
         
-        // Inicializa el número máximo de vocales con el conteo de la primera ventana
         maxNumVowel = countNumVowel;
         
         // Usa la técnica de ventana deslizante para recorrer la cadena
@@ -19,14 +17,15 @@ class Solution {
             if (isVowel(s.charAt(i - k))) {
                 countNumVowel--;
             }
-            
             // Añade el carácter que está entrando en la ventana
             if (isVowel(s.charAt(i))) {
                 countNumVowel++;
             }
             
-            // Actualiza el número máximo de vocales si el conteo actual es mayor
-            maxNumVowel = Math.max(maxNumVowel, countNumVowel);
+            if(maxNumVowel<countNumVowel){
+                maxNumVowel=countNumVowel;
+            }
+           
         }
         
         return maxNumVowel;
@@ -36,3 +35,13 @@ class Solution {
         return "aeiouAEIOU".indexOf(x) != -1;
     }
 }
+
+//explicación: lo que estoy haciendo es pensar en el arreglo como una ventana:
+// [l][e][e][t][c][o][d][e]: si teneemos que k=3, entonces, primero contamos las
+//vocales del primer sub arreglo, después ubicamos un puntero en
+//la posición 3(la letra t), ahí pensamos en que estamos deslizando la ventana, es 
+//decir restamos el valor que aporta l y sumamos el valor que aporta t, en este 
+//caso, como el ejercicio sólo nos pide tomar en cuenta vocales, no hacemos nada,
+//sin embargo, si el ejercicio fuera: [2][3][6][7][2][8][4][5][3], entonces sería
+//más fácil notar que en cada iteración sumamos el elemento de la posición k,
+//y quitamos el de la posición i-k.
