cceb7f5cf6954cb16d92e1e560d8c753af79d950
Russel Bonilla <112036753+soy-russ-bp@users.noreply.github.com>
2024-06-22 00:35:13 -0600

    Time: 4 ms (20.65%), Space: 64.4 MB (30.98%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -10,19 +10,21 @@
  */
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
+        if(head==null || head.next==null){
+            //en el caso de que sólo tengamos un elemento en la lista, entoces:
+            return null ;
+        }
+        
         int middle=(length(head)/2); //devuelve la posición a eliminar
         ListNode current=head;
         
         for(int i=0;i<middle;i++){
             if(i==middle-1){//nos ubicamos en la posición anterior a la que eliminaré
                 current.next=current.next.next;//dejo de referenciar...
-                return head;
             }
             current=current.next; //puntero para avanzar en la lista
         }
-        //en el caso de que sólo tengamos un elemento en la lista, entoces:
-        head = null;
-        return head ;
+        return head;
     }
     
     public int length(ListNode pointer){
