5eba37656d5e504a6ca665c286be9f0bc595ac94
Russel Bonilla <112036753+soy-russ-bp@users.noreply.github.com>
2024-06-25 01:25:48 -0600

    Time: 3 ms (80.37%), Space: 56.1 MB (29.85%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        double sum=0;
+        
+        for(int i=0;i<k;i++){
+            sum +=nums[i];
+        }
+        double mayor= sum;
+       
+        
+        //usar la técnica de slinding window
+        for(int j=k;j<nums.length;j++){
+            sum-=nums[j-k];
+            sum+=nums[j];
+            if(sum>mayor){
+                mayor= sum;
+            }
+        }
+        return mayor/k;
+    }
+}
