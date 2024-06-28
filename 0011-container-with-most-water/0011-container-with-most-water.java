dc9da82b54701ef6c6424dfee9f6a33e79a5dee0
Russel Bonilla <112036753+soy-russ-bp@users.noreply.github.com>
2024-06-28 03:27:18 -0600

    Time: 3 ms (94.56%), Space: 58.1 MB (23.69%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int max=0;
+        int j=height.length - 1; 
+        int i=0;
+        int content=0;
+        
+        while(i<j){
+            
+            if(height[i]<height[j]){
+                content=height[i]*(j-i);
+                i++;
+            }else{
+                content=height[j]*(j-i);
+                j--;
+            }
+            if(max<content){
+                max=content;
+            }
+            
+        }
+        
+        return max;
+    }
+}
