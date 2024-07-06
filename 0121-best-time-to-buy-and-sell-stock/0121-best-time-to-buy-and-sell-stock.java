d1cab59d733314d60bed6a0d0c66b49a56036f38
Russel Bonilla <112036753+soy-russ-bp@users.noreply.github.com>
2024-07-05 23:37:17 -0600

    Time: 2 ms (70.41%), Space: 61.6 MB (50.18%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,8 +1,5 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length == 0) {
-            return 0;
-        }
         
         int minPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
