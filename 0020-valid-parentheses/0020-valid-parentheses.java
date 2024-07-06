460a1ff314a2cc1240f848e60fe81d83ac86e375
Russel Bonilla <112036753+soy-russ-bp@users.noreply.github.com>
2024-07-06 02:44:35 -0600

    Time: 1 ms (99.00%), Space: 41.2 MB (70.92%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,22 +1,23 @@
 import java.util.Stack;
+
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> parentesis = new Stack<>();
         
-        
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{'){
-                parentesis.push(s.charAt(i));
-            }else if(s.charAt(i)==')'){
-                if(parentesis.isEmpty() || parentesis.pop()!='('){
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                parentesis.push(c);
+            } else if (c == ')') {
+                if (parentesis.isEmpty() || parentesis.pop() != '(') {
                     return false;
                 }
-            }else if(s.charAt(i)==']' ){
-                if(parentesis.isEmpty() || parentesis.pop()!='['){
+            } else if (c == ']') {
+                if (parentesis.isEmpty() || parentesis.pop() != '[') {
                     return false;
                 }
-            }else if(s.charAt(i)=='}'){
-                if(parentesis.isEmpty() || parentesis.pop()!='{'){
+            } else if (c == '}') {
+                if (parentesis.isEmpty() || parentesis.pop() != '{') {
                     return false;
                 }
             }
