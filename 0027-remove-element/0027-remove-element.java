56a1e518efe1295f06cd2956db0f941a4acda360
Russel Bonilla <112036753+soy-russ-bp@users.noreply.github.com>
2024-07-13 03:17:47 -0600

    Time: 0 ms (100.00%), Space: 42.5 MB (6.46%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,14 +1,13 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int k = 0; // Número de elementos que no son iguales a val
+        int count=0;
         
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[k] = nums[i];
-                k++;
+        for(int i=0; i< nums.length; i++){
+            if(nums[i]!=val){
+                nums[count]=nums[i];
+                count++;   
             }
         }
-        
-        return k;
+        return count;  
     }
 }
