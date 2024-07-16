9eaab3dbad516543c488918ef83b130101e7eca2
Russel Bonilla <112036753+soy-russ-bp@users.noreply.github.com>
2024-07-15 23:52:03 -0600

    Time: 2 ms (75.02%), Space: 53.1 MB (30.81%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int candidato = nums[0];
+        int contador = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == candidato) {
+                contador++;
+            } else {
+                contador--;
+            }
+
+            if (contador == 0) {
+                candidato = nums[i];
+                contador = 1;
+            }
+        }
+
+        // Dado que siempre hay un elemento mayoritario según el problema, no necesitamos validar más.
+        return candidato;
+    }
+}
