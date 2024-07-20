b973b3851b6da131b218b164bf1c65cc7103c2dc
Russel Bonilla <112036753+soy-russ-bp@users.noreply.github.com>
2024-07-20 01:39:09 -0600

    Time: 0 ms (100.00%), Space: 57.3 MB (42.14%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n=nums.length;
+        k=k%n; //utilizamos el módulo en caso de que k sea mayor que n;
+        
+        rotate(nums, 0, n-1); //rotamos todo el array [1,2,3,4,5,6,7] -> [7,6,5,4,3,2,1]
+        rotate(nums, 0, k-1); //rotamos los primeros k elementos, si k=3 [7,6,5][...] -> [5,6,7][4,3,2,1]
+        rotate(nums, k, n-1); //rotamos los elementos restantes, para tener:[1,2,3,4,5,6,7] -> [5,6,7,1,2,3,4]
+        
+    }
+    
+    public void rotate(int[] nums, int start, int end){ //invierte completamente un subgrupo del arreglo
+        while(start<end){
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            
+            start++;
+            end--;
+        }
+    }
+}
