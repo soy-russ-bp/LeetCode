08da3dc617fcea3678575237e623b0a457e79079
Russel Bonilla <112036753+soy-russ-bp@users.noreply.github.com>
2024-08-03 00:51:24 -0600

    Time: 147 ms (6.71%), Space: 46.4 MB (77.39%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -18,3 +18,32 @@ class Solution {
         return ans;
     }
 }
+
+//código más eficiente de chat, sea i cada binario:
+/*
+Si i es par, entonces countBits(i) = countBits(i / 2).
+Si i es impar, entonces countBits(i) = countBits(i / 2) + 1.
+
+Ejemplo 1: i = 4 (Par)
+Representación binaria de 4 es 100.
+i / 2 = 2, cuya representación binaria es 10.
+countBits(4) = countBits(2) = 1.
+
+
+Ejemplo 2: i = 5 (Impar)
+Representación binaria de 5 es 101.
+i / 2 = 2, cuya representación binaria es 10.
+countBits(5) = countBits(2) + 1 = 2.
+*/
+//class Solution {
+//    public int[] countBits(int n) {
+//        int[] ans = new int[n + 1]; 
+        
+//        for (int i = 1; i <= n; i++) {
+//            ans[i] = ans[i >> 1] + (i & 1);
+            
+//        }
+        
+//        return ans;
+//    }
+//}
